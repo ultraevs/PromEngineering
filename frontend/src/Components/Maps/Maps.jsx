32bed7fa4bc7ec549cf2df8ba1080b51cf7d6e6c
Crowdy <crowwwdy@gmail.com>
2024-04-03 19:08:39 +0300

    Adaptive maps

--- frontend/src/Components/Maps/Maps.jsx
+++ frontend/src/Components/Maps/Maps.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+
 import { YMaps, Map, Placemark, GeolocationControl } from "@pbe/react-yandex-maps";
 
+
 const Maps = () => {
+  let map_width, map_height;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = (event) => {
+      setWidth(event.target.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  if (width <= 768){
+    map_width = 342, 
+    map_height = 207;
+  }
+  else if (width <= 1000){
+    map_width = 417;
+    map_height = 252;
+  }
+  else if (width <= 1280){
+    map_width = 492;
+    map_height = 298;
+  }
+  else if (width <= 1440){
+    map_width = 601;
+    map_height = 364;
+  }
+  else{
+    map_width = 710;
+    map_height = 430;
+  }
+  
   return (
     <div className="contacts_map">
       <YMaps>
         <Map
-          style={{ marginTop: 42, borderRadius: 12, overflow: "hidden", width: 710, height: 430}}
+          style={{borderRadius: 12, overflow: "hidden", width: map_width, height: map_height}}
           defaultState={{
             center: [55.784405, 37.673421],
             zoom: 15,
