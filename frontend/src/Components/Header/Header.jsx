6550fbf5eabdd95fd838ecb66e1bd487d0b61cf5
Konstantin <kostya1703tsoy@gmail.com>
2024-04-05 12:22:07 +0300

    add burger menu with bug

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import Logo from "../../assets/images/TMX_LOGO.svg";
 import Navbar from "../Navbar/Navbar";
 import { animateScroll as scroll } from "react-scroll";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -16,7 +30,8 @@ const Header = () => {
         <div onClick={scrollToTop} className="logo">
           <img src={Logo} alt="TMX logo" />
         </div>
-        <Navbar />
+        <Navbar state={isOpen} setState={setIsOpen} width={windowWidth} />
+        <button className={styles.header__button} onClick={() => setIsOpen(!isOpen)}>Click</button>
       </div>
     </header>
   );
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,68 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import classNames from "classnames";
 import { Link, animateScroll as scroll } from "react-scroll";
 
-const Navbar = () => {
+const Navbar = ({ state, setState, width }) => {
   const scrollToTop = () => {
+    if (width <= 768) {
+      setState(!state);
+    }
     scroll.scrollToTop();
   };
 
+  const handleClick = () => {
+    if (width <= 768) {
+      setState(!state);
+    }
+  };
+
   return (
-    <nav className={styles.header__navbar}>
-      <ul>
-        <li>
-          <div onClick={scrollToTop}>Главная</div>
-        </li>
-        <li>
-          <Link to="services" spy smooth>Услуги</Link>
-        </li>
-        <li>
-          <Link to="projects" spy smooth>Наши проекты</Link>
-        </li>
-        <li>
-          <Link to="customers" spy smooth>Наши заказчики</Link>
-        </li>
-      </ul>
-    </nav>
+    <div
+      className={
+        state
+          ? classNames(styles.header__navbar__block, styles.active)
+          : styles.header__navbar__block
+      }
+      onClick={() => setState(false)}
+    >
+      {state && <div className={styles.blur}></div>}
+      <nav
+        className={styles.header__navbar}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* <div onClick={scrollToTop}>Главная</div>
+        <Link onClick={handleClick} to="services" spy smooth>
+          Услуги
+        </Link>
+        <Link onClick={handleClick} to="projects" spy smooth>
+          Наши проекты
+        </Link>
+        <Link onClick={handleClick} to="customers" spy smooth>
+          Наши заказчики
+        </Link> */}
+        <ul>
+          <li>
+            <div onClick={scrollToTop}>Главная</div>
+          </li>
+          <li>
+            <Link onClick={handleClick} to="services" spy smooth>
+              Услуги
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} to="projects" spy smooth>
+              Наши проекты
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} to="customers" spy smooth>
+              Наши заказчики
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
